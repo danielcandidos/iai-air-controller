553df9d0c4e0b56e6c5cf17a0ccb7c198a7c5fe4
danielcandidos <dcs2@cesar.org.br>
2020-10-08 05:12:19 -0300

    First version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import openSocket from 'socket.io-client';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { Button, Card, CardActions, CardContent, Grid, List, ListItem, Paper, Typography } from '@material-ui/core';
+import { myTheme } from './theme.js';
 import './App.css';
 
-function App() {
+const WhiteTypography = withStyles({
+  root: {
+    color: "#fff"
+  }
+})(Typography);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '42ch',
+    display: 'inline-block',
+  },
+  card: {
+    minWidth: 275,
+    backgroundColor: myTheme.primary.light,
+  },
+  title: {
+    fontSize: 14,
+    color: 'white',
+  },
+  paper: {
+    textAlign: 'center',
+    color: myTheme.primary.contrastText,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+  const lista = [1,2,3,4,5];
+  const [voltage, setVoltage] = useState('no timestamp yet');
+
+  useEffect(() => {
+    const socket = openSocket('http://api.sma.flavindias.com.br/', {transports: ['websocket', 'polling', 'flashsocket']});
+    socket.on("ufpe/cin/in1116/2020/devices/air_conditioner", data => console.log(data));
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: myTheme.primary.main, height: '100vh', margin: 0 }}>
+      <List className={classes.root} style={{maxHeight: '100%', overflow: 'auto'}}>
+        {lista.map(item => {
+          return(
+          <ListItem alignItems="flex-start">
+            <Card className={classes.card}>
+              <CardContent>
+                <Grid container spacing={3}>
+                  <Grid item xs={12}>
+                    <WhiteTypography variant="caption">Time: {new Date().toLocaleString()}</WhiteTypography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <WhiteTypography>Value: {voltage}</WhiteTypography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <WhiteTypography><strong>Voltage:</strong> 221 {item}</WhiteTypography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <WhiteTypography><strong>Amparage:</strong> 6.21</WhiteTypography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <WhiteTypography><strong>Load:</strong> 50</WhiteTypography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <WhiteTypography><strong>BTU:</strong> 9000</WhiteTypography>
+                  </Grid>
+                </Grid>
+              </CardContent>
+              {/* <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions> */}
+            </Card>
+          </ListItem>
+        )})}
+      </List>
     </div>
   );
 }
-
-export default App;
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const myTheme = createMuiTheme({
+  primary: {
+    main: '#674ea7ff',
+    light: '#de801f',
+    contrastText: '#fff',
+    dark: '#000',
+  }
+});
